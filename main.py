a8d0bb44ec12e604656551e490349d0fb7c95d9b
BeataNowakowska <the.beata.nowakowska@gmail.com>
2025-05-12 12:19:06 +0000

    Dodałam Team Leada

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, Beataaaaa")
+def pole_kwadratu(bok):
+  return bok * bok
+
+def pole_prostokąta(a, b):
+  return a * b
+
+#----------------------------------------------
+
+mowic_dalej = True
+while(mowic_dalej):
+  if input("Opowiedzieć ci irytujący żart?") != "tak":
+    mowic_dalej = False
+    

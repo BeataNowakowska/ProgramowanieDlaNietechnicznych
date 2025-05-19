435062c98a49c1b58f1a1625b72b908979ccbd89
BeataNowakowska <the.beata.nowakowska@gmail.com>
2025-05-19 12:46:34 +0000

    Funkcje

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from datetime import date
 
+imie = "Agata"
 
 def moje_imie():
-  imie = "Beata"
   print("Mam na imie", imie)
   print("---------------------------")
 
+def twoje_imie():
+  twoje_imie = input("Jak masz na imie? ")
+  print("Masz na imie", twoje_imie)
+
+  if twoje_imie == imie:
+    print("O masz na imie tak jak ja", imie)
+  else:
+    print("Twoje imie jest różne niż moje",imie)
+
+moje_imie()
+twoje_imie()

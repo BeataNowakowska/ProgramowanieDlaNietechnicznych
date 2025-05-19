a08ad568edc7c9da8c5a619a2d55f52a43e765e4
BeataNowakowska <the.beata.nowakowska@gmail.com>
2025-05-19 11:53:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,3 @@ def moje_imie():
   print("Mam na imie", imie)
   print("---------------------------")
 
-
-def moj_wiek():
-  wiek = 18
-  print("Mam", wiek, "lat")
-
-
-def dzisiaj():
-  dzisiaj = date.today()
-  print("Dzisiaj jest", dzisiaj)
-
-
-def moje_urodziny():
-  dzisiaj = date.today()
-  moje_urodziny = date(1982, 2, 27)
-
-  print("Moje urodziny sa", moje_urodziny)
-  print("Od moich urodzi minęło", dzisiaj - moje_urodziny)
-
-
-def twoje_imie():
-  twoje_imie = input("Jak masz na imie? ")
-  print("Twoje imie to", twoje_imie)

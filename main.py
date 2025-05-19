f685d1191210149a9ca0e9211fdf28d33a11db45
BeataNowakowska <the.beata.nowakowska@gmail.com>
2025-05-19 06:49:20 +0000

    Czysta wersja na poczatek

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-def pole_kwadratu(bok):
-  return bok * bok
-
-def pole_prostokąta(a, b):
-  return a * b
-
-#----------------------------------------------
-
-mowic_dalej = True
-while(mowic_dalej):
-  if input("Opowiedzieć ci irytujący żart?") != "tak":
-    mowic_dalej = False
-    
+print("Hello, World!")

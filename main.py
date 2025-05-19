10aa3e32e6ce455de4f41188b33c822dcc871891
EwaLabenda <labenda.ewa@gmail.com>
2025-05-19 12:49:32 +0000

    ewa

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ def moje_imie():
   print("Mam na imie", imie)
   print("---------------------------")
 
+def twoje_imie():
+  twoje_imie = input("Jak masz na imię?")
+  print("Cześć", twoje_imie)
+                     
+if twoje_imie == moje_imie:
+  print("Mamy takie same imiona")
+else: 
+  print("cześć", twoje_imie)
+
+moje_imie ()
+twoje_imie ()
